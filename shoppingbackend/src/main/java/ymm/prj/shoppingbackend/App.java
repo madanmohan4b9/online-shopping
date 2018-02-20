ef6f31a28d5f9c906c3536452cf1ca13eee0a49e
madan <yarammadanmohan@gmail.com>
2018-02-20 22:50:32 +0530

    My project setup completed Succesfully.

---
+++ shoppingbackend/src/main/java/ymm/prj/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package ymm.prj.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
